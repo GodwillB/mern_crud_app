2320f20a530f7af2d14eeb5869e1ea51d380c080
misheckgithongo <misheckgithongo@gmail.com>
2021-03-31 14:47:52 +0300

    server build

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+app.use('/', express.static('client/build'))
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://belva-digital-library:belva-digital-library@belva-digital-library.3ybao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
