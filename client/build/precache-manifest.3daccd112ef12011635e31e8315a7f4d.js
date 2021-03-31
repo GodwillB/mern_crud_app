53e6e6a5cf2c9079d6c06ac25d1929f766eb4a60
misheckgithongo <misheckgithongo@gmail.com>
2021-03-31 13:55:19 +0300

    deploy

---
+++ client/build/precache-manifest.3daccd112ef12011635e31e8315a7f4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f028143a65671696478eead06d8e87f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4316cb8cca2b130e1272",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "5f69ea4e1595d8d74660",
+    "url": "/static/js/2.4d6c96b3.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.4d6c96b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4316cb8cca2b130e1272",
+    "url": "/static/js/main.918b6c13.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
